55fe3c1dd2b1e51a8a704fd3775d25fe3fe036ab
MalinCrona <malincrona@Malins-MacBook.local>
2016-09-05 14:29:16 +0200

    Har nu testat loopar
    Så här fint blev det

---
+++ README.py
@@ -0,0 +1,8 @@
+print ('Vi gör ett nytt försök')
+print ('Malin vill testa mer')
+t=35
+print (t)
+for x in range(20): print(x)
+for d in [10,11,12,13,14,15]: print(d) 
+print (' hej alla ' * 10)
+print (' hej och hå ' *10)
